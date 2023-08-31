8e372d1876abd4e5cb3c704af3d2b1b5702b3e30
riyms <riyms@DESKTOP-2Q9HL8D>
2023-08-31 17:24:34 +0300

    LocalDate-DogumgunuOdevi

---
+++ src/com/bilgeadam/week09/lecture004/DogumGunuOdev/DogumGunu.java
@@ -0,0 +1,54 @@
+package com.bilgeadam.week09.lecture004.DogumGunuOdev;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
+public class DogumGunu {
+	/**
+	 * Dışarıdan String olarak doğumgünü alınız.
+	 * 
+	 * daha sonra doğduğunuz günden bu güne kadar 
+	 * * kaç yıl, 
+	 * * kaç ay, 
+	 * * kaç hafta,
+	 * * kaç gün, geçtiğini hesaplayıp yazdırın.
+	 * 
+	 * Bir sonraki doğumgününe kaç gün kaldığını hesaplayın.
+	 * 
+	 */
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Lütfen doğduğunuz yılı giriniz: ");
+		int yil = sc.nextInt();
+		System.out.print("Lütfen doğduğunuz ayı giriniz: ");
+		int ay = sc.nextInt();
+		System.out.print("Lütfen doğduğunuz günü giriniz: ");
+		int gun = sc.nextInt();
+		System.out.println();
+		System.out.println("Doğumgününüz: " + LocalDate.of(yil, ay, gun));
+
+		System.out.println();
+		LocalDate gunumuz = LocalDate.now();
+		System.out.println("Günümüzün tarihi: " + gunumuz);
+
+		System.out.println();
+		long kacYilGecmis = ChronoUnit.YEARS.between(LocalDate.of(yil, ay, gun), gunumuz);
+		System.out.println("Doğumgününüzden bugüne " + kacYilGecmis + " yıl geçmiştir.");
+		long kacAyGecmis = ChronoUnit.MONTHS.between(LocalDate.of(yil, ay, gun), gunumuz);
+		System.out.println("Doğumgününüzden bugüne " + kacAyGecmis + " ay geçmiştir.");
+		long kacGunGecmis = ChronoUnit.DAYS.between(LocalDate.of(yil, ay, gun), gunumuz);
+		System.out.println("Doğumgününüzden bugüne " + kacGunGecmis + " gün geçmiştir.");
+
+		System.out.println();
+		LocalDate birSonrakiDogumgunu = LocalDate.of(yil, ay, gun).withYear(gunumuz.getYear());
+		if (birSonrakiDogumgunu.isBefore(gunumuz) || birSonrakiDogumgunu.isEqual(gunumuz)) {
+			birSonrakiDogumgunu = birSonrakiDogumgunu.plusYears(1);
+		}
+		long yeniDogumgunu = ChronoUnit.DAYS.between(gunumuz, birSonrakiDogumgunu);
+		System.out.println("Bir sonraki doğumgününüze " + yeniDogumgunu + " gün kalmıştır.");
+
+	}
+
+}
